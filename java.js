1158f7ab2bdb2539d0ab69f23601de1eaf8f3626
Cesar Carvajal <acarvajal@uninorte.edu.co>
2021-03-05 15:10:48 -0500

    https://github.com/cesarcarvajalt/clubdeportivounioncucuta

--- java.js
+++ java.js
@@ -1,11 +1,13 @@
-var imagenes=new Array(['imagenes/fabio.JPG','imagenes/fabio2.JPG'],
-['imagenes/german.JPG','imagenes/german2.JPG'],
-['imagenes/tellez.JPG','imagenes/tellez2.JPG'],);
+var imagenes=new Array(['imagenes/fabio.JPG','imagenes/fabio2.JPG',"ENTRENADOR"],
+['imagenes/german.JPG','imagenes/german2.JPG',"ENTRENADOR"],
+['imagenes/tellez.JPG','imagenes/tellez2.JPG',"ENTRENADOR"],
+['imagenes/andelfo.JPG','imagenes/andelfo2.JPG',"PSICOLOGO"],);
 var contador=-1;
  function rotarImagenes(){
      contador++
      document.getElementById("varia").src=imagenes[contador%imagenes.length][0];
-      document.getElementById("varias").src=imagenes[contador%imagenes.length][1]
+      document.getElementById("varias").src=imagenes[contador%imagenes.length][1];
+      document.getElementById("text").innerHTML=imagenes[contador%imagenes.length][2]
      }
 onload=function(){
     rotarImagenes();
